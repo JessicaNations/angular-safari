4f26d1086a09c344ac76472bde0b7621f8f141c9
Jessica Nations <jnations@uncomn.com>
2019-08-29 14:06:01 -0500

    less

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
   createDb() {
     const calendars = [
-      { id: 11, name: 'Easter' },
-      { id: 12, name: 'Mothers Day' },
-      { id: 13, name: 'Meme Day' },
-      { id: 14, name: 'Fathers Day' },
-      { id: 15, name: 'Ind Day' },
-      { id: 16, name: 'Labor Day' },
-      { id: 17, name: 'Halloween' },
-      { id: 18, name: 'Vets Day' },
-      { id: 19, name: 'Thxgiving' },
-      { id: 20, name: 'Christmas' }
+      { id: 11, name: 'Day' },
+      { id: 12, name: 'Day' },
+      { id: 13, name: 'Day' },
+      { id: 14, name: 'Day' },
+      { id: 15, name: 'Day' },
+      { id: 16, name: 'Day' },
+      { id: 17, name: 'Day' },
+      { id: 18, name: 'Day' },
+      { id: 19, name: 'Day' },
+      { id: 20, name: 'Day' }
     ];
     return { calendars };
   }
