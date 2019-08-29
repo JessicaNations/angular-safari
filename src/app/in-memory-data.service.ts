75732dd542b2f36af2ad591b7531015bed4cb229
Jessica Nations <jnations@uncomn.com>
2019-08-29 14:04:30 -0500

    word space

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,13 +3,13 @@ export class InMemoryDataService {
     const calendars = [
       { id: 11, name: 'Easter' },
       { id: 12, name: 'Mothers Day' },
-      { id: 13, name: 'Memorial Day' },
+      { id: 13, name: 'Meme Day' },
       { id: 14, name: 'Fathers Day' },
-      { id: 15, name: 'Independence Day' },
+      { id: 15, name: 'Ind Day' },
       { id: 16, name: 'Labor Day' },
       { id: 17, name: 'Halloween' },
-      { id: 18, name: 'Veterans Day' },
-      { id: 19, name: 'Thanksgiving' },
+      { id: 18, name: 'Vets Day' },
+      { id: 19, name: 'Thxgiving' },
       { id: 20, name: 'Christmas' }
     ];
     return { calendars };
