d1ab92b451fad08958d535f41e173cf39e8690b0
Jessica Nations <jnations@uncomn.com>
2019-09-12 11:03:02 -0500

    expansion panels are lame

--- src/app/hours/hours.component.ts
+++ src/app/hours/hours.component.ts
@@ -1,13 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-hours',
   templateUrl: './hours.component.html',
 })
-export class HoursComponent {
-
-// idk trying it out
-// And try this ng-template[matExpansionPanelContent]
-@Input()
-collapsedHeight: string
-}
+export class HoursComponent {}
