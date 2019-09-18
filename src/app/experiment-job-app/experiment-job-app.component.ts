d10b511fb7777403f3c5b6b3227e86d85518ff33
Jessica Nations <jnations@uncomn.com>
2019-09-18 13:46:12 -0500

    dot slash

--- src/app/experiment-job-app/experiment-job-app.component.ts
+++ src/app/experiment-job-app/experiment-job-app.component.ts
@@ -3,6 +3,6 @@ import {Component} from '@angular/core';
 /** @title Simple form field */
 @Component({
   selector: 'my-experiment-job-app',
-  templateUrl: 'experiment-job-app.component.html',
+  templateUrl: './experiment-job-app.component.html',
 })
 export class ExperimentJobAppComponent {}
