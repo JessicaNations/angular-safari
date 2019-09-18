982980b3412cafd5c77bccc3f393fcd78efa518e
Jessica Nations <jnations@uncomn.com>
2019-09-18 13:51:05 -0500

    geez bugs man

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ import { GeneralAdmissionComponent } from './general-admission/general-admission
 import { GroupRatesComponent } from './group-rates/group-rates.component';
 import { FieldTripsComponent } from './field-trips/field-trips.component';
 import { JobAppComponent } from './job-app/job-app.component';
-import { ExperimentJobAppComponent } from './experiment-job-app.component';
+import { ExperimentJobAppComponent } from './experiment-job-app/experiment-job-app.component';
 
 
 const routes: Routes = [
