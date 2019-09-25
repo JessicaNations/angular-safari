5ddc8a59569f02fcfa5b1487f79dda82d0fb8ec8
Jessica Nations <jnations@uncomn.com>
2019-09-25 08:38:09 -0500

    woot it works but out of margin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ import { JobAppComponent } from './job-app/job-app.component';
 
 import { NgImageSliderModule } from './ng-image-slider.module';
 import { MyMaterialModule } from './material.module';
-// import { SliderMainComponent } from './slider-main/slider-main.component';
+import { SliderMainComponent } from './slider-main/slider-main.component';
 // import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 
 
@@ -85,7 +85,7 @@ import { MyMaterialModule } from './material.module';
     GroupRatesComponent,
     FieldTripsComponent,
     JobAppComponent,
-    // SliderMainComponent
+    SliderMainComponent
 
   ],
   // providers: [CalendarService],
