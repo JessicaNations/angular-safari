2d5132a1bb54f138e213dcc1d5ff63d6461fa8ac
Jessica Nations <jnations@uncomn.com>
2019-09-25 14:50:18 -0500

    2 more

--- src/app/ng-image-slider.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-    selector: 'app-ng-image-slider',
-    templateUrl: './ng-image-slider.component.html',
-    styleUrls: ['./ng-image-slider.component.scss'],
-    encapsulation: ViewEncapsulation.None
-})
-export class NgImageSliderComponent {}
