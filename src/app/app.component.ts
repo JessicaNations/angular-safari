80f0a0f7b6eb8d12b9fcd978e47a04e0b388955f
Jessica Nations <jnations@uncomn.com>
2019-09-25 14:47:07 -0500

    whoops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: 'app.component.html'
   // encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
