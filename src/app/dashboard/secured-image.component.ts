9c4a73b0cc1e1c5c637dc1ff6eebac86f0e08deb
Jessica Nations <jnations@uncomn.com>
2019-09-26 11:02:02 -0500

    trying secure gifs

--- src/app/dashboard/secured-image.component.ts
+++ src/app/dashboard/secured-image.component.ts
@@ -13,19 +13,38 @@ import { map, switchMap } from 'rxjs/operators';
 export class SecuredImageComponent implements OnChanges  {
   @Input() private src: string;
   private src$ = new BehaviorSubject(this.src);
-  dataUrl$ = this.src$.pipe(switchMap(url => this.loadImage(url)));
+//   dataUrl$ = this.src$.pipe(switchMap(url => this.loadImage(url)));
 
 
-  constructor(private httpClient: HttpClient, private domSanitizer: DomSanitizer) {
-  }
+//   constructor(private httpClient: HttpClient, private domSanitizer: DomSanitizer) {
+//   }
 
-  ngOnChanges(): void {
-      this.src$.next(this.src);
-    }
+//   ngOnChanges(): void {
+//       this.src$.next(this.src);
+//     }
 
-  private loadImage(url: string): Observable<any> {
-    return this.httpClient
-      .get(url, {responseType: 'blob'})
-      .pipe(map(e => this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(e))));
-  }
+//   private loadImage(url: string): Observable<any> {
+//     return this.httpClient
+//       .get(url, {responseType: 'blob'})
+//       .pipe(map(e => this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(e))));
+//   }
+// }
+ngOnChanges(): void {
+  this.src$.next(this.src);
+}
+
+// this stream will contain the actual url that our img tag will load
+// everytime the src changes, the previous call would be canceled and the
+// new resource would be loaded
+dataUrl$ = this.src$.pipe(switchMap(url => this.loadImage(url)))
+
+// we need HttpClient to load the image and DomSanitizer to trust the url
+constructor(private httpClient: HttpClient, private domSanitizer: DomSanitizer) {
+}
+
+private loadImage(url: string): Observable<any> {
+  return this.httpClient
+    .get(url, {responseType: 'blob'})
+    .pipe(map(e => this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(e))))
+}
 }
